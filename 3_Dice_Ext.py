fd5a54d0c33f9552d88b5d6afc332af61f525cff
MiguelAnhalt <miguelangel.lopezmejia@students.hs-anhalt.de>
2025-04-08 13:46:01 +0200

    Final Version

--- 3_Dice_Ext.py
+++ 3_Dice_Ext.py
@@ -84,7 +84,7 @@ class DiceGenerator:
         self.results = [random.randint(1, 6) for roll in range(self.num_rolls)]
 
 
-    def count_ocurrences_per_result(self):
+    def count_ocurrences_per_face(self):
         """
         Counts the occurrences of each die face in the results and logs the statistics.
 
@@ -119,9 +119,12 @@ class DiceGenerator:
             >>> sim.count_ocurrences_per_result()
             INFO:__main__: Face 1: Appears 1 times - (1.0 %)
         """
+        logger.info("-----------------------------------------------------------------------------------------------")
         self.counts = Counter(self.results) # Counter({4: 23, 5: 20, 1: 17, 2: 16, 3: 15, 6: 9})
         for i in range(1, 7):
             logger.info(f" Face {i}: Appears {self.counts[i]} times - ({round(self.counts[i]/self.num_rolls*100,2)} %) ")
+        
+        logger.info("-----------------------------------------------------------------------------------------------")
         
         
     def test_randomness(self):
@@ -156,16 +159,10 @@ class DiceGenerator:
         difference between what is observed and what is expected is "too large" to be attributed 
         to chance, 0.05 is the standar - Check for reference Images/3_Dice_Chi_square_probability_table.png)
 
-        3. P-Value = 0.05 (We consider the value of P-Value to be 0.05, is an indicator that tells us what is the probability of 
-        observing a test statistic as extreme or more extreme than the one calculated, under the Null hypothesis.)
-
         Conclusion:
         - We defined the Null Hypothesis = The dice is fair, which means that the frequencies of each face of the dice are the same.
         - Null Hypotesis expects χ2 > Critic Value
         - As the value of χ2 < Critic Value (6.0 < 11.07) we can't reject the Null Hyphotesis (The dice is fair)
-        - We can also use the P-value where, is p < 0.05, we can reject the Null Hyphotesis, 
-        - is p > 0.05 we can't reject the Null Hypothesis 
-        - P-value will be 0.3062, which means 0.3062 > 0.05 (The dice is fair)
         - The expected mean is 2
         - The observed mean is 3.666
         """
@@ -177,7 +174,11 @@ class DiceGenerator:
    
         # Calcular chi-cuadrado manualmente
         chi_square_stat = np.sum((observed - expected) ** 2 / expected)
-                
+
+        logger.info("-----------------------------------------------------------------------------------------------")
+        logger.info(f"-------------------------- Test of Randomness - Chi-square method -----------------------------")
+        logger.info("-----------------------------------------------------------------------------------------------")
+
         if chi_square_stat > 11.07:
             logger.info(f" Degrees of freedom: 5 (6 faces - 1)")
             logger.info(" Compare with critical value (e.g., 11.07 for alpha=0.05, df=5)")
@@ -188,70 +189,10 @@ class DiceGenerator:
             logger.info(" Compare with critical value (e.g., 11.07 for alpha=0.05, df=5)")            
             logger.info(f" The value of Chi-square ({chi_square_stat}) < Critic Value (11.07)")
             logger.info(" The conclusion is that there is no enough information to reject the Null hypothesis")
-
-
-    def chi_square(self):
-        """
-        The Chi-Square method is used to determine if there is a significant difference between the expected 
-        frequencies and the observed frequencies, the function assumes that each face of the dice the 
-        same probability of 1/6.
-
-        The method is represented by the following formula:
-            χ2=∑ (Oi-Ei)2/Ei
-
-        Where 
-        Oi is the observed frequency, in this case, the frequency of each face of the dice
-        Ei is the excpected frecuency of each face of the dice
-
-        E.G.
-        Analysis:
-        If the dice is rolled 12 times, we expect that the frequency of the values to be like:
-        Expected = 12/6 = 2 for each face
-        Observed = [0,4,1,3,3,1] (sum is 12)
-
-        χ2 = 6.0 (In this case, the formula will calculate the Chi-Square for [0,4,1,3,3,1] and [2,2,2,2,2,2])
         
-        Now, we need to consider the following:
+        logger.info("-----------------------------------------------------------------------------------------------")
+        logger.info("-----------------------------------------------------------------------------------------------")
 
-        1. Degrees of freedom = 6-5 = 1 (We know that there are 6 categories, if we roll the dice 12 times, 
-        the sum of the frequencies of each face of the dice must be 12, if we know that the values of the 
-        numbers 1-5 are 0,4,1,3,3 the value of the number 6 must be 1 so they sum 12, there is no freedom 
-        to chosse the last value, that is why we have 1 degree of freedom.)
-
-        2. Critic Value = 11.07 (Considering a significance level (α) = 0.05 , It is the threshold defined to decide when a 
-        difference between what is observed and what is expected is "too large" to be attributed 
-        to chance, 0.05 is the standar - Check for reference Images/3_Dice_Chi_square_probability_table.png)
-
-        3. P-Value = 0.05 (We consider the value of P-Value to be 0.05, is an indicator that tells us what is the probability of 
-        observing a test statistic as extreme or more extreme than the one calculated, under the Null hypothesis.)
-
-        Conclusion:
-        - We defined the Null Hypothesis = The dice is fair, which means that the frequencies of each face of the dice are the same.
-        - Null Hypotesis expects χ2 > Critic Value
-        - As the value of χ2 < Critic Value (6.0 < 11.07) we can't reject the Null Hyphotesis (The dice is fair)
-        - We can also use the P-value where, is p < 0.05, we can reject the Null Hyphotesis, 
-        - is p > 0.05 we can't reject the Null Hypothesis 
-        - P-value will be 0.3062, which means 0.3062 > 0.05 (The dice is fair)
-        - The expected mean is 2
-        - The observed mean is 3.666
-        """
-        # Chi-square test for uniformity
-        expected = self.num_rolls / 6  # Expected frequency for each number
-        observed = [self.counts[i] for i in range(1, 7)] # Observed frequencies per face of the dice
-
-        chi_square_stat, p_value = stats.chisquare(observed, [expected] * 6)
-        
-
-        logger.info(" The Null Hypothesis is that the dice is fair.")
-
-        if chi_square_stat > 11.07 and p_value < 0.05:
-            logger.info(f" The value of Chi-square ({chi_square_stat}) > Critic Value (11.07), and the P-value ({round(p_value,2)}) < 0.05.")
-            logger.info(" The conclusion is that the Null hypothesis can be rejected.")
-        else:
-            logger.info(f" The value of Chi-square ({chi_square_stat}) < Critic Value (11.07), and the P-value ({round(p_value,2)}) > 0.05.")
-            logger.info(" The conclusion is that there is no enough information to reject the Null hypothesis")
-
-    
     def mean_graph(self):
         """
         Generates a bar chart of dice roll results and overlays the observed mean.
@@ -267,11 +208,12 @@ class DiceGenerator:
             and a configured `logger`.
 
         """       
+        
         indices = range(len(self.results)) # Obtain the indexes for the X-axis
         logger.info(f" The Expected mean is 3.5")
         mean_dices = mean(self.results)
         logger.info(f" The Observed mean is {mean_dices}")
-
+        logger.info("-----------------------------------------------------------------------------------------------")
         # Create bar chart
         plt.bar(indices, self.results, color = 'cyan', label = 'Values')
         # Add a horizontal line for the mean
@@ -346,11 +288,9 @@ def main():
 
     dice_generator.roll_dice()
 
-    dice_generator.count_ocurrences_per_result()
+    dice_generator.count_ocurrences_per_face()
     
-    # Extra analysis
     dice_generator.test_randomness()
-
 
     dice_generator.mean_graph()
 
